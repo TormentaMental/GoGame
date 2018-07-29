a1a370169c4e94913d759a6555d900f396836086
Gaston Nan <nangastonr@gmail.com>
2018-07-29 12:01:54 -0300

    php-cs-fix

--- src/BoardFactory.php
+++ src/BoardFactory.php
@@ -6,28 +6,28 @@ use GoGame\Board;
 
 class BoardFactory
 {
-  const SMALL_BOARD_SIZE = 9;
-  const MEDIUM_BOARD_SIZE = 30;
-  const BIG_BOARD_SIZE = 90;
+    const SMALL_BOARD_SIZE = 9;
+    const MEDIUM_BOARD_SIZE = 30;
+    const BIG_BOARD_SIZE = 90;
   
-  public static function createSmallBoard(): Board
-  {
-    return self::create('small');
-  }
+    public static function createSmallBoard(): Board
+    {
+        return self::create('small');
+    }
   
-  public static function createMediumBoard(): Board
-  {
-    return self::create('medium');
-  }
+    public static function createMediumBoard(): Board
+    {
+        return self::create('medium');
+    }
   
-  public static function createBigBoard(): Board
-  {
-    return self::create('big');
-  }
+    public static function createBigBoard(): Board
+    {
+        return self::create('big');
+    }
   
-  public static function create($size): Board
-  {
-    switch ($size) {
+    public static function create($size): Board
+    {
+        switch ($size) {
       case 'small':
         return new Board(self::buildIntersections(self::SMALL_BOARD_SIZE));
       break;
@@ -41,19 +41,17 @@ class BoardFactory
       default:
         return self::create(self::MEDIUM_BOARD_SIZE);
       break;
-    }    
-  }
+    }
+    }
   
-  private static function buildIntersections($size): array
-  {
-    $intersections = [];
-    for ($i=0; $i<$size; $i++) {
-      for ($g=0; $g<$size; $g++) {
-        $intersections[$i][$g] = '';
-      }
-    }    
-    return $intersections;
-  }
+    private static function buildIntersections($size): array
+    {
+        $intersections = [];
+        for ($i=0; $i<$size; $i++) {
+            for ($g=0; $g<$size; $g++) {
+                $intersections[$i][$g] = '';
+            }
+        }
+        return $intersections;
+    }
 }
-
-
